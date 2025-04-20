cf6e8be6e8fde6adb433d2a42f8fb670927b6801
Ali Kerem Kol <ali.demirtas283@gmail.com>
2025-04-20 21:08:43 +0300

    Delete Toyota32bitProje/Project directory

--- Toyota32bitProje/Project/rates-consumer/rates-consumer/src/main/java/com/mydomain/consumer/rates_consumer/repository/RatesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mydomain.consumer.rates_consumer.repository;
-
-import com.mydomain.consumer.rates_consumer.model.TblRates;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RatesRepository extends JpaRepository<TblRates, Long> {
-}
