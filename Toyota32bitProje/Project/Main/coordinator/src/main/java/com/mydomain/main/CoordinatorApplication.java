ced7b2055f7bf6e4b8a610ead81c8fb57ba45c35
Ali Kerem Kol <ali.demirtas283@gmail.com>
2025-05-28 20:04:56 +0300

    "AppConfig" ve "HttpService" sınıfları kaldırıldı. Artık uygulama kurulumu "main" metodunda yapılıyor.

--- Toyota32bitProje/Project/Main/coordinator/src/main/java/com/mydomain/main/CoordinatorApplication.java
+++ Toyota32bitProje/Project/Main/coordinator/src/main/java/com/mydomain/main/CoordinatorApplication.java
@@ -1,36 +1,56 @@
 package com.mydomain.main;
 
-import com.mydomain.main.config.AppConfig;
-import com.mydomain.main.coordinator.ICoordinator;
+import com.mydomain.main.config.ConfigReader;
+import com.mydomain.main.coordinator.Coordinator;
+import com.mydomain.main.exception.ProviderInitializationException;
+import com.mydomain.main.service.KafkaProducerService;
+import com.mydomain.main.service.RateCalculatorService;
+import com.mydomain.main.service.RedisConsumerService;
+import com.mydomain.main.service.RedisProducerService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import redis.clients.jedis.JedisPool;
 
-/**
- * CoordinatorApplication, bağımsız koordinatör uygulamasının giriş noktasıdır.
- * ConfigReader aracılığıyla yapılandırmayı yükler, koordinatör ve sağlayıcıları başlatır
- * ve uygulamanın çalışmaya devam etmesini sağlar.
- */
+
 public class CoordinatorApplication {
 
     private static final Logger logger = LogManager.getLogger(CoordinatorApplication.class);
 
-    /**
-     * Uygulamayı başlatır:
-     * 1) Log ile başlama mesajı yazar,
-     * 2) AppConfig ile ICoordinator örneğini oluşturur,
-     * 3) Ana iş parçacığını sonsuza dek bekleterek uygulamanın çalışmasını sürdürür.
-     *
-     * @param args komut satırı argümanları (kullanılmıyor)
-     */
+
     public static void main(String[] args) {
         logger.info("=== Starting Coordinator... ===");
 
-        // Build all: coordinator, providers
-        ICoordinator coordinator = AppConfig.init();
+        // Redis bağlantılarını ve servisleri başlat
+        JedisPool jedisPool = new JedisPool(ConfigReader.getRedisHost(), ConfigReader.getRedisPort());
 
-        logger.info("=== CoordinatorMain is up and running. ===");
+        RedisProducerService redisRawProducer = new RedisProducerService(jedisPool,ConfigReader.getRawStreamName(),ConfigReader.getStreamMaxLen());
+        RedisConsumerService redisRawConsumer = new RedisConsumerService(jedisPool,ConfigReader.getRawStreamName(),ConfigReader.getRawGroup(),ConfigReader.getRawConsumerName(),ConfigReader.getStreamReadCount(),ConfigReader.getStreamBlockMillis());
 
-        // Keep running
+        RedisProducerService redisCalculatedProducer = new RedisProducerService(jedisPool,ConfigReader.getCalcStreamName(),ConfigReader.getStreamMaxLen());
+        RedisConsumerService redisCalculatedConsumer = new RedisConsumerService(jedisPool,ConfigReader.getCalcStreamName(),ConfigReader.getCalcGroup(),ConfigReader.getCalcConsumerName(),ConfigReader.getStreamReadCount(),ConfigReader.getStreamBlockMillis());
+
+        // Hesaplama servisini başlat
+        RateCalculatorService rateCalculatorService = new RateCalculatorService();
+
+        // Kafka üretici servisini başlat
+        KafkaProducerService kafkaProducerService = new KafkaProducerService();
+
+        // Coordinator'ı başlat
+        // bu arada bunu ICoordinator olarak mı kullanmalıyım ?? ama o zamanda "loadProviders" ve "stratRateStreamPipeline" metodlarını kullanamam
+        Coordinator coordinator = new Coordinator(redisRawProducer, redisRawConsumer, redisCalculatedProducer, redisCalculatedConsumer, rateCalculatorService, kafkaProducerService);
+
+        try {
+            coordinator.loadProviders(ConfigReader.getProviders());
+        } catch (ProviderInitializationException pie) {
+            logger.fatal("Provider init fatal: {}", pie.getMessage(), pie);
+        }
+
+        /* --- Hesaplama + Kafka döngüsünü başlat --- */
+        coordinator.startRateStreamPipeline();
+
+        logger.info("=== Coordinator up & running ===");
+
+        /* Main thread’i canlı tut */
         try {
             Thread.currentThread().join();
         } catch (InterruptedException e) {
